fd1efcc8db23acb265b426f7529f771aa8f72f96
AzureFirehawk <madison.thomas094@gmail.com>
2025-11-15 09:29:00 -0600

    routes added

--- routes/stars.js
+++ routes/stars.js
@@ -1,1 +1,20 @@
 const router = require('express').Router();
+const validate = require('../middleware/validate');
+
+const starsController = require('../controllers/stars');
+
+router.get('/', starsController.getAll);
+
+router.get('/:id', starsController.getSingle);
+
+router.post(
+    '/',
+    validate.validatePost,
+    handleErros(starsController.createPost)
+);
+
+router.put('/:id', starsController.updatePost);
+
+router.delete('/:id', starsController.deletePost);
+
+module.exports = router;
