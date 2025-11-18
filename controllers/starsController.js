bd2aff0171b3437ceba4d99736f1783b106ed791
AzureFirehawk <madison.thomas094@gmail.com>
2025-11-18 14:57:02 -0600

    error handling response update

--- controllers/starsController.js
+++ controllers/starsController.js
@@ -1,16 +1,16 @@
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
-const getAll = async (req, res) => {
+const getAll = async (req, res, next) => {
     try {
         const stars = await mongodb.getDb().db().collection('stars').find().toArray();
         res.status(200).json(stars);
     } catch (err) {
-        res.status(500).json({ message: err.message });
+        next(err);
     }
 };
 
-const getSingle = async (req, res) => {
+const getSingle = async (req, res, next) => {
     try {
         const id = new ObjectId(req.params.id);
         const star = await mongodb.getDb().db().collection('stars').findOne({ _id: id });
@@ -19,24 +19,28 @@ const getSingle = async (req, res) => {
         }
         res.status(200).json(star);
     } catch (err) {
-        res.status(500).json({ message: err.message });
+        next(err);
     }
 };
 
 
 const createStar = async (req, res) => {
-    const star = {
-        name: req.body.name,
-        distance: req.body.distance,
-        radius: req.body.radius,
-        mass: req.body.mass,
-        spectralClass: req.body.spectralClass
-    }
-    const response = await mongodb.getDb().db().collection('stars').insertOne(star);
-    if (response.acknowledged) {
-        res.status(201).json(response);
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while creating the entry.');
+    try {
+        const star = {
+            name: req.body.name,
+            distance: req.body.distance,
+            radius: req.body.radius,
+            mass: req.body.mass,
+            spectralClass: req.body.spectralClass
+        }
+        const response = await mongodb.getDb().db().collection('stars').insertOne(star);
+        if (response.acknowledged) {
+            res.status(201).json(response);
+        } else {
+            res.status(500).json(response.error || 'Some error occurred while creating the entry.');
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
 };
 
