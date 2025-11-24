e0b0e16c9fc1c3973ff92c59cbe92ea2af20aa8f
AzureFirehawk <madison.thomas094@gmail.com>
2025-11-24 11:28:54 -0600

    authenticate middleware

---
+++ middleware/authenticate.js
@@ -0,0 +1,8 @@
+const isAuth = (req, res, next) => {
+    if (req.session.user === undefined) {
+        return res.status(401).json({ message: 'you do not have access' });
+    }
+    next();
+};
+
+module.exports = isAuth;
