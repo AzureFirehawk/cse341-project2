332ffb8116ad5f512d1a0f61058d64215d0d0fe4
AzureFirehawk <madison.thomas094@gmail.com>
2025-11-24 12:03:24 -0600

    removed unnecessary files

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,8 @@
 const router = require('express').Router();
 const passport = require('passport');
 
-// router.use('/', (req, res) => {
-//     res.send('Hello World!');
-// })
-
 router.use('/stars', require('./stars'));
 router.use('/planets', require('./planets'));
-router.use('/users', require('./accounts'));
 
 router.get('/login', passport.authenticate('github'), (req, res) => {});
 router.get('/logout', function(req, res){
