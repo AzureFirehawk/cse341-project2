37c44270023b7ba73c4e63b45a37be67943b5142
AzureFirehawk <madison.thomas094@gmail.com>
2025-11-24 12:03:13 -0600

    capitalization

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,6 +1,6 @@
 const isAuth = (req, res, next) => {
     if (req.session.user === undefined) {
-        return res.status(401).json({ message: 'you do not have access' });
+        return res.status(401).json({ message: 'You do not have access' });
     }
     next();
 };
